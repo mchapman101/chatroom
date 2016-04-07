5c9ea8005a371c95f37aa25dcf22649625188609
MarkChapman <mchapman101@yahoo.com>
2016-04-06 18:46:45 -0600

    chatroom

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,22 +1,48 @@
 var app = angular.module('chatroom');
 
-app.controller('mainCtrl', function($scope, parseService){
-  //In your controller you'll have a getParseData function and a postData function, but should be placed on $scope.
+app.controller('mainCtrl', function($scope, parseService) {
 
-  //The getParseData function will call the getData method on the parseService object. You'll then save the result of that request to 
-  //your controllers $scope as messages ($scope.messages)
+$scope.getData = function() {
+    parseService.getData().then(karamFilter).then(function(response) {
+        $scope.messages = response;
+
+    });
+}
+$scope.postData = function() {
+    parseService.postData($scope.message).then(function() {
+        $scope.message = "";
+        $scope.getData();
+    })
+};
+
+setInterval(function() {
+    $scope.getData();
+}, 1500)
+
+$scope.formatDate = function(dateString) {
+    return new Date(dateString).toLocaleString();
+}
+
+var karamFilter = function(response) {
+    // var repeatedWords =/([A-Za-z\u00C0-\u1FFF\u2800-\uFFFD]+)\s+\1(?:\s|$)/gi;
+    for (var i = 0; i < response.length; i++) {
+        if (!response[i].text) {
+            response.splice(i, 1);
+            i--;
+        }
+        // else if (response[i].text.match(repeatedWords) {
+        else if (response[i].text.toLowerCase().indexOf("karam") > -1) {
+            response.splice(i, 1);
+            i--;
+        }
+
+}
+
+// console.log(response);
+return response;
+}
 
 
 
-  //The postData function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
-  //pass that text to the postData method on the parseService object which will then post it to the parse backend.
-
-
-
-
-  //uncomment this code when your getParseData function is finished
-  //This goes and gets new data every second, which mimicking a chat room experience.
-  // setInterval(function(){
-  //   $scope.getParseData();
-  // }, 1500)
+//end
 })
